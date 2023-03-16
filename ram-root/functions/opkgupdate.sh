043fed050baa30d4f9d724a630aa56e9a55f8e1f
faruktezcan <66576976+faruktezcan@users.noreply.github.com>
2023-03-16 09:32:04 -0600

    Add files via upload

--- ram-root/functions/opkgupdate.sh
+++ ram-root/functions/opkgupdate.sh
@@ -1,32 +1,36 @@
 opkg_update () {
   local time=0
-  local interactive=$1
+  local interactive=${1}
   local dir=$(grep lists_dir /etc/opkg.conf | awk '{print $3}')
+  local f1 f2 f3
 
   [[ $# -ge 1 && "$interactive" != "-i" ]] && interactive="-i"
   [[ -d $dir ]] && [[ -n "$(ls -1A $dir)" ]] && let time=$(date +%s)-$(date -r $dir +%s)
   [[ $time -ge 600 ]] && time=0
 
   if [[ $time -eq 0 ]]; then
+    if ( ! ping -c 1 downloads.openwrt.org &>/dev/null ); then return 1; fi
+
     echo -e "\a\e[1mUpdating repositories\e[0m\n"
-    local f1 f2 f3
     local opkgdir=/tmp/opkg-lists
     mkdir $opkgdir &>/dev/null
     cd $opkgdir
+
     while read f1 f2 f3; do
       echo -n "$f2..."
-      if ln -f $opkgdir/$f2 $opkgdir/Packages.gz &>/dev/null; then
+      if ( ln -f $opkgdir/$f2 $opkgdir/Packages.gz &>/dev/null ); then
         wget -q $f3/Packages.gz 2>&1
       else
-        wget -qO $opkgdir/$f2 $f3/Packages.gz 2>&1
+        wget -q -O $opkgdir/$f2 $f3/Packages.gz 2>&1
       fi
-      if ln -f $opkgdir/$f2.sig $opkgdir/Packages.sig &>/dev/null; then
+      if ( ln -f $opkgdir/$f2.sig $opkgdir/Packages.sig &>/dev/null ); then
         wget -q $f3/Packages.sig 2>&1
       else
-        wget -qO $opkgdir/$f2.sig $f3/Packages.sig 2>&1
+        wget -q -O $opkgdir/$f2.sig $f3/Packages.sig 2>&1
       fi
       echo -e "\e[1mdone\e[0m"
     done < /etc/opkg/distfeeds.conf
+
     rm $opkgdir/Packages.* &>/dev/null
     echo
   fi

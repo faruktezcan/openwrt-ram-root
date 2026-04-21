d584f08682ab8394992c8cc0079823572241fbe6
faruktezcan <66576976+faruktezcan@users.noreply.github.com>
2026-04-20 21:12:35 -0600

    Add files via upload

--- ram-root/functions/opkgupdate.sh
+++ ram-root/functions/opkgupdate.sh
@@ -1,39 +1,37 @@
 opkg_update () {
+  local _OK_='\033[0;32m\xe2\x9c\x93\033[0m'
+  local _FAIL_='\033[0;31m\xe2\x9c\x97\033[0m'
+  local __OK__='\033[0;32m[\xe2\x9c\x93]\033[0m'
+  local __FAIL__='\033[0;31m[\xe2\x9c\x97]\033[0m'
+  local _WARNING_='\033[0;33mWARNING\033[0m'
+  local _ERROR_='\033[0;31mERROR\033[0m'
+
+  local _COL_GRN="\e[32m"
+  local _COL_BLU="\e[34m"
+  local _COL_RED="\e[31m"
+  local _COL_RST="\e[0m"
+
   local time=0
   local interactive=${1}
-  local dir=$(grep lists_dir /etc/opkg.conf | awk '{print $3}')
-  local f1 f2 f3
 
   [[ $# -ge 1 && "$interactive" != "-i" ]] && interactive="-i"
-  [[ -d $dir ]] && [[ -n "$(ls -1A $dir)" ]] && let time=$(date +%s)-$(date -r $dir +%s)
+  local count=$(echo $(ls /tmp/cache/apk/) | wc -w )
+  [[ -d /tmp/cache/apk && $count -gt 0 ]] && time=$(( $(date +%s) - $(date -r /tmp/cache/apk +%s) ))
+
   [[ $time -ge 600 ]] && time=0
 
   if [[ $time -eq 0 ]]; then
-    if ( ! ping -c 1 downloads.openwrt.org &>/dev/null ); then return 1; fi
+    if ! ( ping -s 1 -c 1 downloads.openwrt.org &>/dev/null ); then
+      echo -e "${_COL_RED}No internet connection${_COL_RST}"
+      return 1;
+    fi
 
-    echo -e "\a\e[1mUpdating repositories\e[0m\n"
-    local opkgdir=/tmp/opkg-lists
-    mkdir $opkgdir &>/dev/null
-    cd $opkgdir
-
-    while read f1 f2 f3; do
-      echo -n "$f2..."
-      if ( ln -f $opkgdir/$f2 $opkgdir/Packages.gz &>/dev/null ); then
-        wget -q $f3/Packages.gz 2>&1
+    echo -e "${_COL_GRN}Updating repositories${_COL_RST}"
+      if ( apk update ); then
+        echo -e ${_OK_}
       else
-        wget -q -O $opkgdir/$f2 $f3/Packages.gz 2>&1
+        echo -e ${_FAIL_}
       fi
-      if ( ln -f $opkgdir/$f2.sig $opkgdir/Packages.sig &>/dev/null ); then
-        wget -q $f3/Packages.sig 2>&1
-      else
-        wget -q -O $opkgdir/$f2.sig $f3/Packages.sig 2>&1
-      fi
-      echo -e "\e[1mdone\e[0m"
-    done < /etc/opkg/distfeeds.conf
-
-    rm $opkgdir/Packages.* &>/dev/null
     echo
   fi
-
-  return 0
 }
